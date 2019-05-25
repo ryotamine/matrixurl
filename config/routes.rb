6075b139cdc432a38373bb41de6222e489be219f
Ryota Mine <ryotamine@gmail.com>
2019-05-25 23:51:34 +0000

    Configure short URL route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "shortened_urls#index"
+  get "/:short_url", to: "shortened_urls#show"
+  get "shortened/:short_url", to: "shortened_urls#shortened", as :shortened
+  post "/shortened_urls/create"
+  get "/shortened_urls/fetch_original_url"
 end
