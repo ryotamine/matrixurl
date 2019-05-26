80b5c4e7c53ca269d79173fb429160b66a87435e
Ryota Mine <ryotamine@gmail.com>
2019-05-26 00:23:18 +0000

    Fix database typo

--- db/migrate/20190525224811_create_shortened_urls.rb
+++ db/migrate/20190525224811_create_shortened_urls.rb
@@ -1,9 +1,9 @@
 class CreateShortenedUrls < ActiveRecord::Migration[5.2]
   def change
     create_table :shortened_urls do |t|
-      t.text: long_url
-      t.string: short_url
-      t.string: sanitize_url
+      t.text :long_url
+      t.string :short_url
+      t.string :sanitize_url
       t.timestamps
     end
   end
